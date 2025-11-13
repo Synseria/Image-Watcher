a6d992a5ef75ef3faca0b29e229556c235824663
Synseria <synseria@gmail.com>
2025-11-13 13:54:03 +0100

    Met à jour la configuration de Prettier et désactive l'intégration avec ESLint ; ajoute pino-pretty comme dépendance

--- eslint.config.js
+++ eslint.config.js
@@ -34,7 +34,7 @@ export default [
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      'prettier/prettier': 'error',
+      'prettier/prettier': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
     }
