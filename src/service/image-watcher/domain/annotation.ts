6b84115ff0b4ac858442ea65fcef243231d4eb15
Synseria <synseria@gmail.com>
2025-11-14 13:16:54 +0100

    Refactor les énumérations en objets constants pour TypeAnnotation, TypeMode, TypeStrategy et TypeParam Ajout du paramètre env override

--- src/service/image-watcher/domain/annotation.ts
+++ src/service/image-watcher/domain/annotation.ts
@@ -1,52 +1,65 @@
 /**
  * Enumération des annotations image-watcher
  */
-export enum TypeAnnotation {
-  MODE = 'image-watcher/mode',
-  STRATEGY = 'image-watcher/strategy',
-  LAST_UPDATED = 'image-watcher/last-updated',
-  LAST_UPDATED_VERSION = 'image-watcher/last-updated-version',
-  LAST_NOTIFIED = 'image-watcher/last-notified',
-  LAST_NOTIFIED_VERSION = 'image-watcher/last-notified-version',
-  PREVIOUS_VERSION = 'image-watcher/previous-version',
-  CURRENT_VERSION = 'image-watcher/current-version',
-  RELEASE_URL = 'image-watcher/release-url',
-  TOKEN_UPDATE = 'image-watcher/token-update'
-}
+export const TypeAnnotation = {
+  WATCH: 'image-watcher/watch',
+  MODE: 'image-watcher/mode',
+  STRATEGY: 'image-watcher/strategy',
+  LAST_UPDATED: 'image-watcher/last-updated',
+  LAST_UPDATED_VERSION: 'image-watcher/last-updated-version',
+  LAST_NOTIFIED: 'image-watcher/last-notified',
+  LAST_NOTIFIED_VERSION: 'image-watcher/last-notified-version',
+  PREVIOUS_VERSION: 'image-watcher/previous-version',
+  CURRENT_VERSION: 'image-watcher/current-version',
+  RELEASE_URL: 'image-watcher/release-url',
+  TOKEN_UPDATE: 'image-watcher/token-update',
+} as const;
+
+// eslint-disable-next-line no-redeclare
+export type TypeAnnotation = typeof TypeAnnotation[keyof typeof TypeAnnotation];
 
 /**
  * Enumération des modes
  */
-export enum TypeMode {
-  AUTO_UPDATE = 'AUTO_UPDATE',
-  NOTIFICATION = 'NOTIFICATION',
-  DISABLED = 'DISABLED',
-  DEFAULT = 'DEFAULT'
-}
+export const TypeMode = {
+  AUTO_UPDATE: 'AUTO_UPDATE',
+  NOTIFICATION: 'NOTIFICATION',
+  DISABLED: 'DISABLED',
+  DEFAULT: 'DEFAULT',
+} as const;
+
+// eslint-disable-next-line no-redeclare
+export type TypeMode = typeof TypeMode[keyof typeof TypeMode];
 
 /**
  * Stratégie de mise à jour
  */
-export enum TypeStrategy {
-  ALL = 'ALL',
-  MAJOR = 'MAJOR',
-  MINOR = 'MINOR',
-  PATCH = 'PATCH'
-}
+export const TypeStrategy = {
+  ALL: 'ALL',
+  MAJOR: 'MAJOR',
+  MINOR: 'MINOR',
+  PATCH: 'PATCH',
+} as const;
+// eslint-disable-next-line no-redeclare
+export type TypeStrategy = typeof TypeStrategy[keyof typeof TypeStrategy];
 
 /**
- * Enumération des notificationq
+ * Enumération des notifications
  */
-export enum TypeParam {
-  INTERNAL = 'INTERNAL',
-  CONFIGURATION = 'CONFIGURATION'
-}
+export const TypeParam = {
+  INTERNAL: 'INTERNAL',
+  CONFIGURATION: 'CONFIGURATION'
+} as const;
+
+// eslint-disable-next-line no-redeclare
+export type TypeParam = typeof TypeParam[keyof typeof TypeParam];
 
 /**
  * Interface représentant les annotations pour une application
  */
 export interface ApplicationAnnotation {
   /** Données */
+  [TypeAnnotation.WATCH]?: boolean;
   [TypeAnnotation.MODE]: TypeMode;
   [TypeAnnotation.STRATEGY]: TypeStrategy;
   [TypeAnnotation.CURRENT_VERSION]?: string;
